8af6e0b5524f70d5d8d142e67839fe97900d260e
jagan mandava <jmr.mandava@gmail.com>
2021-04-20 03:25:07 +0530

    Perfect Number

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,15 @@
-def is_armstrong_number(number):
-    pass
+def is_armstrong_number(self):
+
+    order = len(str(self))
+    sum = 0
+    temp = self
+
+    while temp >= 0:
+        digit = temp % 10
+        sum += digit ** order
+        temp //= 10
+
+        if self == sum:
+            return True
+        else:
+            return False
