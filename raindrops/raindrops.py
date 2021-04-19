963dee65fdfdca17e0399d0b38d9da94607f1652
jagan mandava <jmr.mandava@gmail.com>
2021-04-20 03:28:20 +0530

    Raindrops

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -2,7 +2,7 @@ def convert(number: int) -> str:
     x = ""
 
     if number % 3 == 0:
-        x = x + "Pling"
+        x += "Pling"
     if number % 5 == 0:
         x = x + "Plang"
     if number % 7 == 0:
