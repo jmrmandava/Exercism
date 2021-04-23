ae8220ccc4e333de9fac1ba137283f1f5930540d
jagan mandava <jmr.mandava@gmail.com>
2021-04-23 21:06:47 +0530

    Resistor-color

--- resistor-color/resistor_color.py
+++ resistor-color/resistor_color.py
@@ -1,6 +1,36 @@
+a = {'Black': 0,
+     'Brown': 1,
+     'Red': 2,
+     'Orange': 3,
+     'Yellow': 4,
+     'Green': 5,
+     'Blue': 6,
+     'Violet': 7,
+     'Grey': 8,
+     'White': 9}
+
+
 def color_code(color):
-    pass
+    if color == 'black':
+        return (a.get('Black'))
+    if color == "white":
+        return (a.get('White'))
+    if color == "orange":
+        return (a.get('Orange'))
+
+
+b = ["black",
+     "brown",
+     "red",
+     "orange",
+     "yellow",
+     "green",
+     "blue",
+     "violet",
+     "grey",
+     "white",
+     ]
 
 
 def colors():
-    pass
+    return b
