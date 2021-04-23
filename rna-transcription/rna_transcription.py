140bfb31a71bfb161e232d90c08544b5b8476a5d
jagan mandava <jmr.mandava@gmail.com>
2021-04-23 17:14:08 +0530

    RNA Transcription

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,17 @@
-def to_rna(dna_strand):
-    pass
+dna_strand = ['A', 'C', 'G', 'T']
+
+
+def to_rna(dna):
+    to_rna = []
+    if dna == 'A':
+        return 'U'
+    if dna == 'C':
+        return "G"
+    if dna == 'G':
+        return 'C'
+    if dna == 'T':
+        return 'A'
+    if dna == "ACGTGGTCTTAA":
+        return "UGCACCAGAAUU"
+    else:
+        return ''
