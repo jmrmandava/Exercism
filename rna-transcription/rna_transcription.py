32d4921a5816ab6d994be3904cfa6e64da27ef0e
jagan mandava <jmr.mandava@gmail.com>
2021-04-28 18:02:30 +0530

    RNA transcription resubmitted after implementing the code using  dictionary

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,17 +1,11 @@
-dna_strand = ['A', 'C', 'G', 'T']
-
-
 def to_rna(dna):
-    to_rna = []
-    if dna == 'A':
-        return 'U'
-    if dna == 'C':
-        return "G"
-    if dna == 'G':
-        return 'C'
-    if dna == 'T':
-        return 'A'
-    if dna == "ACGTGGTCTTAA":
-        return "UGCACCAGAAUU"
-    else:
-        return ''
+    a = {'A': 'U',
+         'C': 'G',
+         'G': 'C',
+         'T': 'A'}
+
+    b = ''
+
+    for i in dna:
+        b += a.get(i)
+    return b
