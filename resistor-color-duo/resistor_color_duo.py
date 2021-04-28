29f562a266f00d03cf26cb2dec0fdb9b9e42ada1
jagan mandava <jmr.mandava@gmail.com>
2021-04-28 18:50:01 +0530

    Resistor-color-duo code is completed and submitted to exercism

--- resistor-color-duo/resistor_color_duo.py
+++ resistor-color-duo/resistor_color_duo.py
@@ -1,13 +1,15 @@
-a = {'Black': 0,
-     'Brown': 1,
-     'Red': 2,
-     'Orange': 3,
-     'Yellow': 4,
-     'Green': 5,
-     'Blue': 6,
-     'Violet': 7,
-     'Grey': 8,
-     'White': 9}
-
-
 def value(color):
+    a = {'black': 0,
+         'brown': 1,
+         'red': 2,
+         'orange': 3,
+         'yellow': 4,
+         'green': 5,
+         'blue': 6,
+         'violet': 7,
+         'grey': 8,
+         'white': 9}
+    b = ''
+    for i in color[:2]:
+        b = b + str(a[i])
+    return int(b)
