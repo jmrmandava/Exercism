127c09aad6a4dfff2cedd25d75570bdc86609ab3
jagan mandava <jmr.mandava@gmail.com>
2021-04-30 17:35:50 +0530

    Darts code submission completed

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,15 @@
+import math
+from math import sqrt
+
+
 def score(x, y):
-    pass
+    a = sqrt(x**2 + y**2)
+
+    if a <= 1:
+        return 10
+    elif a <= 5:
+        return 5
+    elif a <= 10:
+        return 1
+    else:
+        return 0
