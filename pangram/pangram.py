dd9bc7f22343b58983cad27bcfc7c59ca9db3ff2
jagan mandava <jmr.mandava@gmail.com>
2021-05-29 22:32:50 +0530

    Pangram resubmitted by the suggestion of mentor

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,7 +1,8 @@
 def is_pangram(sentence):
     a = "abcdefghijklmnopqrstuvwxyz"
+    input = sentence.lower()
     for x in a:
-        if x not in sentence.lower():
+        if x not in input:
             return False
 
     return True
